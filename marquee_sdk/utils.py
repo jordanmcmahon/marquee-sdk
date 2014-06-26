173e3ea86feb79e441d6e4edd953e28be99c2ce8
Alex Cabrera <alex@marquee.by>
2014-06-26 02:06:43 -0400

    minor provisioning fixes, remove print statements

--- marquee_sdk/utils.py
+++ marquee_sdk/utils.py
@@ -4,9 +4,7 @@ import  jinja2
 import  os
 
 template_loader = jinja2.FileSystemLoader(TEMPLATE_DIR)
-print template_loader
 template_env    = jinja2.Environment(loader=template_loader)
-print template_env
 
 def copy_template(src, dest, **kwargs):
     template = template_env.get_template(src)
