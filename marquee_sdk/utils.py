6c1037fa7680aa98d529eebfd9761d7058033849
Alex Cabrera <alex@marquee.by>
2014-06-26 01:46:08 -0400

    [startproject] ops templates

--- marquee_sdk/utils.py
+++ marquee_sdk/utils.py
@@ -1,11 +1,17 @@
-from    .config     import *
+from .config import *
+
 import  jinja2
+import  os
 
 template_loader = jinja2.FileSystemLoader(TEMPLATE_DIR)
+print template_loader
 template_env    = jinja2.Environment(loader=template_loader)
+print template_env
 
 def copy_template(src, dest, **kwargs):
     template = template_env.get_template(src)
     contents = template.render(**kwargs)
+
     with open(dest, 'w') as f:
         f.write(contents)
+
