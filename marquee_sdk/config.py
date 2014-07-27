a3f72e36b6f7068a50136fe6cc427c8aeb0c3684
Alex Cabrera <alex@marquee.by>
2014-07-27 14:18:11 -0400

    fix deploys by making a `.slack` config file optional

--- marquee_sdk/config.py
+++ marquee_sdk/config.py
@@ -1,6 +1,7 @@
 from os.path import dirname, expanduser, join
 
 SDK_DIR                 = dirname(__file__)
+SLACK_CONFIG            = join(SDK_DIR, '.slack')
 DIGITAL_OCEAN_CONFIG    = join(SDK_DIR, '.digitalocean')
 TEMPLATE_DIR            = join(SDK_DIR, 'scaffolding')
 PUB_KEY                 = join(expanduser('~'), '.ssh', 'id_rsa.pub')
